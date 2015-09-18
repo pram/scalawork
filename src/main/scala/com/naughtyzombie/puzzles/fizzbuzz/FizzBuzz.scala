16a79e9c69edf1e2999c3a412338ef045a5952fd
Pram Attale <pram@attale.com>
2015-09-18 21:45:58 +0100

    removing unused code

--- src/main/scala/com/naughtyzombie/puzzles/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/com/naughtyzombie/puzzles/fizzbuzz/FizzBuzz.scala
@@ -5,16 +5,6 @@ package com.naughtyzombie.puzzles.fizzbuzz
  */
 object FizzBuzz {
 
-  //Not very testable code
-  def checkFizzBuzz(x: Int) = {
-    (1 until x).map(i => (i % 3, i % 5) match {
-      case (0, 0) => "FizzBuzz"
-      case (0, _) => "Fizz"
-      case (_,0) => "Buzz"
-      case _ => i
-    }).foreach(println)
-  }
-
   def getFizzBuzz(x: Int): String = {
     (x % 3, x % 5) match {
       case (0, 0) => "FizzBuzz"
